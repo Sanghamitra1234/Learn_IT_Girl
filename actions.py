a2735e5ac68846b2219e0e65e5ca49f15bdd06b2
Sanghamitra1234 <sanghota4567@gmail.com>
2019-04-27 16:30:04 +0530

    hidden action file added

---
+++ actions.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+from secret import *
+
+from rasa_core_sdk import Action
+from rasa_core_sdk.events import SlotSet
+
+
+class ActionWeather(Action):
+    def name(self):
+        return 'action_weather'
+
+    def run(self, dispatcher, tracker, domain):
+        from apixu.client import ApixuClient
+        api_key = 'c3e1096f0ccc478cb40101824192704'
+        client = ApixuClient(api_key)
+
+        loc = tracker.get_slot('location')
+        current = client.current(q=loc)
+
+        country = current['location']['country']
+        city = current['location']['name']
+        condition = current['current']['condition']['text']
+        temperature_c = current['current']['humidity']
+        humidity = current['current']['humidity']
+
+        response = """ It is currently {} in {} at the moment. 
+        The temperature is {} degrees.""" .format(condition, city, temperature_c)
+
+        dispatcher.utter_message(response)
+        return [SlotSet('location', loc)]
